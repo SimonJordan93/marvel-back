11f3f02e202e12c2d9a03ea02e3c26831f100e35
simonjordan93 <simon.m.jordan@gmail.com>
2023-02-16 18:18:33 +0100

    route name fixed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// My Routes
+// My Routes (fixed)
 const charactersRouter = require("./routes/characters");
 const comicsRouter = require("./routes/comics");
 
